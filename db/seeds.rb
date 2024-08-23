db013ccdb37a4fd8c6379d58ad4c0ccd566ceec8
André Muta <andremuta@gmail.com>
2024-08-23 18:06:58 -0300

    create seed with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+require 'faker'
+
+# Clear existing data
+Event.destroy_all
+User.destroy_all
+
+# Create users
+users = []
+10.times do |i|
+  users << User.create!(
+    name: Faker::Name.name,
+    email: "user#{i+1}@example.com",
+    password: 'password'
+  )
+end
+
+# Create events for each user
+users.each do |user|
+  12.times do
+    Event.create!(
+      name: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraph,
+      location: Faker::Address.full_address,
+      start_time: 1.week.from_now,
+      end_time: 2.weeks.from_now + 2.hours,
+      user: user
+    )
+  end
+end
+
+puts "Created #{User.count} users"
+puts "Created #{Event.count} events"
