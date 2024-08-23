463d74026d3f580c0c28b3aecbecdd7e738912e4
André Muta <andremuta@gmail.com>
2024-08-22 22:42:43 -0300

    Add basic factories for user and events with factorybox

---
+++ spec/factories/events.rb
@@ -0,0 +1,11 @@
+# spec/factories/events.rb
+FactoryBot.define do
+  factory :event do
+    sequence(:name) { |n| "Sample Event #{n}" }
+    description { "This is a sample event." }
+    location { "Sample Location" }
+    start_time { DateTime.now }
+    end_time { DateTime.now + 1.hour }
+    association :user
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+# spec/factories/users.rb
+FactoryBot.define do
+  factory :user do
+    sequence(:name) { |n| "John Doe #{n}" }
+    sequence(:email) { |n| "johndoe#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+
+    trait :with_events do
+      transient do
+        events_count { 5 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:event, evaluator.events_count, user: user)
+      end
+    end
+  end
+end
