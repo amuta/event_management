84d64073950283b14e937b48be01d718d5f7e1b1
André Muta <andremuta@gmail.com>
2024-08-22 23:02:17 -0300

    Add rspec for user model with basic validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:user) }
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
 end
