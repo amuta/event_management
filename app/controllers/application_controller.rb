192562636708fafba35f77bf2aa21a0c8e4c6465
André Muta <andremuta@gmail.com>
2024-08-24 13:41:50 -0300

    oops, forgot this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    @current_user = authorize_request
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless current_user
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def paginate(collection)
+    collection.page(params[:page]).per(params[:per_page])
+  end
+
+  private
+
+  def authorize_request
+    token = request.headers['Authorization']
+
+    begin
+      decoded = AuthenticationTokenService.decode(token)
+      
+      User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError, JWT::VerificationError
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
